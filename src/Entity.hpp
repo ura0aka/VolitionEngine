ff65deec1dc64bbeb8d53cc6dca17fd33256dbd8
ura0aka <bitcoinassassination@gmail.com>
2022-08-08 10:46:00 -0400

    Moved component ID system back to component class

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -5,44 +5,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
-#include <vector>
-#include <array>
-#include <memory>
-#include <bitset>
-#include <cstdint>
-#include <cassert>
-
-
 class Component;
-
-// == COMPONENT ID SYSTEM ==
-using ComponentID = std::uint32_t;
-constexpr std::size_t maxComponents{32};
-
-using ComponentBitset = std::bitset<maxComponents>;
-using ComponentArray = std::array<Component*, maxComponents>;
-
-inline ComponentID genUComponentID() noexcept
-{
-    // generate a unique id for a component 
-    // (this gets called in the getComponentTypeID function -> when Entity::addComponent() is called)
-    static ComponentID lastID{0u};
-    return lastID++;
-}
-
-template<typename T> inline ComponentID getComponentTypeID() noexcept
-{
-    // for each unique component type, the template will be instanciated
-    // only once for each type of component thus, creating a unique ID
-
-    // make sure getComponentTypeID only gets called with 'T' that inherits from the Component class
-    // (std::is_base_of provides ::value = TRUE if the derived is part of the given base class)
-    static_assert(std::is_base_of<Component, T>::value && "ERROR: T must inherit from base Component class. \n");
-    static ComponentID typeID{genUComponentID()};
-
-    // subsequent calls with the same component type will return the same ID
-    return typeID;
-}
 
 class Entity
 {
